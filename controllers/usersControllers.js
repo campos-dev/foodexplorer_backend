576bff68ab28e1dcbc5b051f7f13bec5a4607715
Marcelo Campos <camposwebdev@gmail.com>
2023-10-10 18:39:14 -0300

    handling errors

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,6 +1,13 @@
+const AppError = require("../utils/AppError");
+
 class UsersControllers {
   create(req, res) {
     const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      throw new AppError("All fields are mandatories!");
+    }
+
     res.json({ name, email, password });
   }
 }
