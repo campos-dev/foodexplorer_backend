bb5ab47c11ce65981d98365d7949b35f2789af4b
Marcelo Campos <camposwebdev@gmail.com>
2023-10-18 14:08:08 -0300

    added cors

--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("../utils/AppError");
 const uploadConfig = require("./configs/upload");
 
+const cors = require("cors");
 const express = require("express");
 const app = express();
-app.use(express.json());
 
+app.use(cors());
+app.use(express.json());
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
 const routes = require("./routes");
