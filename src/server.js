5837d8869668d13551c345b1bdea9981de7419fc
Marcelo Campos <camposwebdev@gmail.com>
2023-10-31 15:18:45 -0300

    updated cors origin and readme with deploy link

--- src/server.js
+++ src/server.js
@@ -9,7 +9,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: [
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+      "https://explorer-foodexplorer.netlify.app",
+    ],
     credentials: true,
   })
 );
