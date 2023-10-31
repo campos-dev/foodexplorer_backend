8f16e807d5602f84bcf8a0f0a73e3eec001ccaa6
Marcelo Campos <camposwebdev@gmail.com>
2023-10-31 15:55:52 -0300

    set cors headers

--- src/server.js
+++ src/server.js
@@ -7,6 +7,15 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(
   cors({
     origin: [
