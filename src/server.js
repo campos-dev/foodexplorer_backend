472e39b8ed7b968b7a538180131117b188a805b7
Marcelo Campos <camposwebdev@gmail.com>
2023-11-01 18:04:07 -0300

    config default cors

--- src/server.js
+++ src/server.js
@@ -7,12 +7,7 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://explorer-foodexplorer.netlify.app",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
