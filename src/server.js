871e531286f53f3c918bcc5735df2d9bd663dc23
Marcelo Campos <camposwebdev@gmail.com>
2023-11-06 17:49:04 -0300

    changed order server code using the rocketseat template

--- src/server.js
+++ src/server.js
@@ -1,52 +1,44 @@
-require("dotenv/config");
 require("express-async-errors");
-const AppError = require("../utils/AppError");
-const uploadConfig = require("./configs/upload");
-
-const cookieParser = require("cookie-parser");
 
 const cors = require("cors");
 const express = require("express");
-const app = express();
+const routes = require("./routes");
+const cookieParser = require("cookie-parser");
 
+const AppError = require("../utils/AppError");
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://explorer-foodexplorer.netlify.app",
+    origin: ["https://explorer-foodexplorer.netlify.app"],
     credentials: true,
   })
 );
 
-app.use(express.json());
-app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
-
-app.use(cookieParser());
-
-const routes = require("./routes");
-
 app.use(routes);
 
-app.use((req, res, next) => {
-  if (req.session && req.session.expires < Date.now()) {
-    res.status(401).send("Session expired");
-  } else {
-    next();
-  }
-});
+require("dotenv/config");
 
-app.use((error, req, res, next) => {
-  if (error instanceof AppError) {
-    return res
-      .status(error.statusCode)
-      .json({ status: "error", message: error.message });
-  }
-  console.error(error);
+const uploadConfig = require("./configs/upload");
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
-  return res
-    .status(500)
-    .json({ status: "error", message: "Internal server error" });
+app.use((err, request, response, next) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
 });
 
 const PORT = 3333;
-app.listen(PORT, "0.0.0.0", () =>
-  console.log(`Server is running on port:${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
