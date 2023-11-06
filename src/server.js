2f9200f8cc42fabcd85e5fade8f2981564eb5653
Marcelo Campos <camposwebdev@gmail.com>
2023-11-06 16:46:21 -0300

    remove / from the end of the origin path

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://explorer-foodexplorer.netlify.app/",
+    origin: "https://explorer-foodexplorer.netlify.app",
     credentials: true,
   })
 );
