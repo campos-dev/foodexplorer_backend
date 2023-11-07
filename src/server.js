392ea64ba294a88dce90f77f201e8e43a99946a8
Marcelo Campos <camposwebdev@gmail.com>
2023-11-07 19:08:39 -0300

    uninstall pm2, add process.env.PORT and host 0000

--- src/server.js
+++ src/server.js
@@ -40,5 +40,7 @@ app.use((err, request, response, next) => {
   });
 });
 
-const PORT = 3333;
-app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server is running on Port ${PORT}`)
+);
